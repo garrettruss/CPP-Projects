73247dbb228bee5c41f0dfb262735d2b2d8c586b
garrettruss <garrettmruss@gmail.com>
2021-11-18 15:31:20 -0500

    save

---
+++ DogYears/dog_years.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    //my dogs age
+    int dog_age = 6;
+    // “The first two years of a dog’s life count as 21 human years.”
+    int early_years = 21;
+    // “Each following year counts as 4 human years.”
+    int later_years = (dog_age - 2) * 4;
+    // converted into human years
+    int human_years = early_years + later_years;
+
+    std::cout << "My name is Riley! I am " << human_years << " years old in human years.";
+}
